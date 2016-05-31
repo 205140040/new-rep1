c926447bfd7295ecfb07b5b8be2387738a94252e
jiangzi <205140040@qq.com>
2016-06-01 06:54:09 +0800

    jd

---
+++ java-design/src/main/java/com/demo/java_design/App.java
@@ -0,0 +1,13 @@
+package com.demo.java_design;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
