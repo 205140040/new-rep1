17fc048d17353cfadb64a3a5933e19a540c4828d
jiangzi <205140040@qq.com>
2016-06-22 00:20:58 +0800

    555

--- dubbo-all/dubbo-provider/src/main/java/com/demo/hello/service/impl/HelloServiceImpl.java
+++ dubbo-all/dubbo-provider/src/main/java/com/demo/hello/service/impl/HelloServiceImpl.java
@@ -1,10 +1,17 @@
 package com.demo.hello.service.impl;
 
-import org.apache.log4j.Logger;
+import java.util.List;
 
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.demo.beans.Student;
+import com.demo.dao.StudentMapper;
 import com.demo.hello.service.IHelloService;
 
 public class HelloServiceImpl implements IHelloService {
+	@Autowired
+	private StudentMapper studentMapper;
 	/*
 	 * (non-Javadoc)
 	 * 
@@ -13,8 +20,9 @@ public class HelloServiceImpl implements IHelloService {
 	private Logger logger = Logger.getLogger(HelloServiceImpl.class);
 
 	@Override
-	public void sayHello(String name) {
+	public List<Student> sayHello(String name) {
 		logger.info("访问了:sayHello");
 		System.out.println(name + ",hello");
+		return studentMapper.selectList();
 	}
 }
