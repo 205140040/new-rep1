6e45a80a12781cf2263751b8ea5ebd00d739c174
jiangzi <205140040@qq.com>
2016-06-21 14:27:03 +0800

    55

--- dubbo-all/dubbo-provider/src/main/java/com/demo/hello/service/impl/HelloServiceImpl.java
+++ dubbo-all/dubbo-provider/src/main/java/com/demo/hello/service/impl/HelloServiceImpl.java
@@ -1,13 +1,20 @@
 package com.demo.hello.service.impl;
 
+import org.apache.log4j.Logger;
+
 import com.demo.hello.service.IHelloService;
 
 public class HelloServiceImpl implements IHelloService {
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see com.demo.hello.service.impl.IHelloService#sayHello(java.lang.String)
 	 */
+	private Logger logger = Logger.getLogger(HelloServiceImpl.class);
+
 	@Override
 	public void sayHello(String name) {
+		logger.info("访问了:sayHello");
 		System.out.println(name + ",hello");
 	}
 }
