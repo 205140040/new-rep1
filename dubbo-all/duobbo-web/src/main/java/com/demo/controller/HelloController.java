7b893fab077c1a95c0a909fa3d04c578210556e9
jiangzi <205140040@qq.com>
2016-06-22 00:26:04 +0800

    555

--- dubbo-all/duobbo-web/src/main/java/com/demo/controller/HelloController.java
+++ dubbo-all/duobbo-web/src/main/java/com/demo/controller/HelloController.java
@@ -1,10 +1,14 @@
 package com.demo.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.alibaba.fastjson.JSON;
+import com.demo.beans.Student;
 import com.demo.hello.service.IHelloService;
 
 @Controller
@@ -16,7 +20,8 @@ public class HelloController {
 	@RequestMapping("/sayHello")
 	@ResponseBody
 	public String sayHello() {
-		helloService.sayHello("酱紫");
-		return "world";
+		List<Student> students = helloService.sayHello("酱紫");
+		System.out.println(JSON.toJSONString(students));
+		return JSON.toJSONString(students);
 	}
 }
