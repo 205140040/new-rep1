28a77b5daa3143720b55131aa23ce0fd131c5205
jiangzi <205140040@qq.com>
2016-06-21 14:26:21 +0800

    6

--- dubbo-all/dubbo-provider/src/main/java/com/demo/hello/service/impl/StartProvider.java
+++ dubbo-all/dubbo-provider/src/main/java/com/demo/hello/service/impl/StartProvider.java
@@ -1,21 +1,12 @@
 package com.demo.hello.service.impl;
 
-import java.io.IOException;
-
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import com.alibaba.dubbo.container.Main;
 
 public class StartProvider {
 
 	public static void main(String[] args) {
-		ApplicationContext context = new ClassPathXmlApplicationContext("spring-context.xml");
-		// context.start();
-		System.out.println("启动成功");
-		try {
-			System.in.read();// 按任意键退出
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		// 使用dubbo容器启动
+		Main.main(args);
 	}
 
 }
