469886e8a23f20241877abaaa769ac57949c558e
jiangzi <205140040@qq.com>
2016-07-07 00:18:00 +0800

    aaaa

--- java-design/src/main/java/com/demo/deep/c18/io/BiaoZhunIoDemo.java
+++ java-design/src/main/java/com/demo/deep/c18/io/BiaoZhunIoDemo.java
@@ -1,10 +1,33 @@
 package com.demo.deep.c18.io;
 
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+
 public class BiaoZhunIoDemo {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws Exception {
+		/**
+		 * 标准输入：输出，错误 system.in system.out system.err
+		 */
+		BufferedInputStream stdin = new BufferedInputStream(System.in);
+		byte[] bs = new byte[stdin.available()];
+		stdin.read(bs);// 读标准输入到 bs字节数组中
+		System.out.println(new String(bs, "UTF-8"));
+		stdin.close();
+		// system.out是printstream
+		/**
+		 * 标准io重定向
+		 * setIn(）
+		 * setOut()
+		 * setErr()
+		 */
+		System.setOut(new PrintStream(new FileOutputStream(new File("C:\\Users\\admin\\Desktop\\tio2.txt"))));
+		PrintStream printStream = new PrintStream(System.out);
+		printStream.println("hello");
+		printStream.close();
+		
 	}
 
 }
