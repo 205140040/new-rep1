d6839dc1c332c31d8072349ca8a2a4ff54da1788
jiangzi <205140040@qq.com>
2016-07-17 21:32:45 +0800

    linux666666666666666666

---
+++ java-design/src/main/java/com/demo/linux/LiDemo.java
@@ -0,0 +1,20 @@
+package com.demo.linux;
+
+public class LiDemo {
+
+	public static void main(String[] args) {
+		/**
+		 * 基础指令的操作:
+		 * 1.显示日期与时间：date
+		 * 2.日历cal 使用方法:cal [month] [year] 显示某年，某月
+		 * 3.计算器bc
+		 * 1.1显示格式化日期:date +%Y-%m-%d
+		 * 4.常用按键
+		 * 4.1tab命令补全
+		 * 4.2ctrl+c中断目前的程序，如输入 find / ，程序一直运行，ctrl+c后可以中断
+		 * 5.man page 查看命令说明，如 man date,
+		 * 此时可用用space键翻页，或输入/ 某个单词，查找相关信息
+		 */
+	}
+
+}
