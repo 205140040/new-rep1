3cd6f49dfb1f4cc6dbfbf0d8b7a83d1ae7613c34
jiangzi <205140040@qq.com>
2016-08-10 22:36:57 +0800

    nginx

---
+++ maven-linux/src/main/java/com/timobb/demo/NginxDemo.java
@@ -0,0 +1,23 @@
+package com.timobb.demo;
+
+public class NginxDemo {
+
+	public static void main(String[] args) {
+		/**
+		 * 1.ngigx 安装http://www.cnblogs.com/skynet/p/4146083.html
+		 * 每次执行./configure 然后执行make 然后执行make install 
+		 * 2.
+		 * http://www.cnblogs.com/jianxie/p/3990377.html
+		 * 2.1nginx启动: sudo
+		 * /usr/local/nginx/sbin/nginx 
+		 * 2.2重启: cd /usr/local/nginx/sbin ./nginx -s
+		 * reload 
+		 * 2.3停止: 查询nginx主进程号
+		 * ps -ef | grep nginx
+		 * 从容停止 kill -QUIT 主进程号 
+		 * 到 31页
+		 *
+		 */
+	}
+
+}
