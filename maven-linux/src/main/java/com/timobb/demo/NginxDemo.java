34f264f41e3736dd2f89784ec3121cd158e4c77d
jiangzi <205140040@qq.com>
2016-08-11 22:44:46 +0800

    aaaaaaaaaaaaaaaaaaaaa

--- maven-linux/src/main/java/com/timobb/demo/NginxDemo.java
+++ maven-linux/src/main/java/com/timobb/demo/NginxDemo.java
@@ -10,13 +10,20 @@ public class NginxDemo {
 		 * http://www.cnblogs.com/jianxie/p/3990377.html
 		 * 2.1nginx启动: sudo
 		 * /usr/local/nginx/sbin/nginx 
-		 * 2.2重启: cd /usr/local/nginx/sbin ./nginx -s
-		 * reload 
+		 * 2.2重启: cd /usr/local/nginx/sbin ./nginx -s  reload 
 		 * 2.3停止: 查询nginx主进程号
 		 * ps -ef | grep nginx
 		 * 从容停止 kill -QUIT 主进程号 
-		 * 到 31页
-		 *
+		 * 第3章:nginx基本配置
+		 * 1.配置文件在conf下,nginx.conf
+		 * 3.2.1虚拟主机
+		 * 3.2.3配置基于域名的虚拟主机
+		 * 3.4开启压缩配置gzip:
+		 * http://www.cnblogs.com/mitang/p/4477220.html
+		 *3.6:nginx的浏览器缓存设置
+		 *第4章：nginx与php的安装
+		 *第5章：nginx与jsp的安装与配置(重要)
+		 *dao 70页
 		 */
 	}
 
