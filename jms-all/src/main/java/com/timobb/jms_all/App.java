17641d0f60d08c8e70006f626c0d75d18049e38d
jiangzi <205140040@qq.com>
2016-08-20 16:35:19 +0800

    jms

---
+++ jms-all/src/main/java/com/timobb/jms_all/App.java
@@ -0,0 +1,13 @@
+package com.timobb.jms_all;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
