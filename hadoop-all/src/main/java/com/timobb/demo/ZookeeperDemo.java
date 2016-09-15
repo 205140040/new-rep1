7ea2910c7d376d3761236b4cd22229b536036747
jiangzi <205140040@qq.com>
2016-09-15 23:02:36 +0800

    6666666666666

---
+++ hadoop-all/src/main/java/com/timobb/demo/ZookeeperDemo.java
@@ -0,0 +1,23 @@
+package com.timobb.demo;
+
+public class ZookeeperDemo {
+
+	public static void main(String[] args) {
+		/**
+		 * 第2章：一致性协议:dao 28
+		 * 第3章:dao 63
+		 * 第4章：zookeeper与paxos dao 70
+		 * 4.2.3深入zab协议,dao 82
+		 * 第5章：使用zookeeper,dao 90
+		 * bin目录下的脚本作用:
+		 * zkCleanup清除zookeeper历史数据，包括日志和事务
+		 * zkCli：zookeeper的一个简易客户端
+		 * zkEnv：设置zookeeper的环境变量
+		 * zkServer:zookeeper的启动，停止和重启
+		 * 如：停止服务: ./zkServer.sh stop
+		 * 5.2客户端脚本:zkCli dao 99
+		 */
+
+	}
+
+}
