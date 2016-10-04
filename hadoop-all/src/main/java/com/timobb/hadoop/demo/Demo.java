27c77f30d314bbb2ceb97af64d956a8b9ef26d13
jiangzi <205140040@qq.com>
2016-10-05 03:27:00 +0800

    aaaaaaaaaaaaaa

--- hadoop-all/src/main/java/com/timobb/hadoop/demo/Demo.java
+++ hadoop-all/src/main/java/com/timobb/hadoop/demo/Demo.java
@@ -3,7 +3,10 @@ package com.timobb.hadoop.demo;
 public class Demo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// 1.6.2 HBase的数据管理 27
+		//第二章：hadoop的安装与配置 33
+		//第三章:hadoop应用案例分析 50
+		//第4章：mapreduce计算模型  69
 
 	}
 
