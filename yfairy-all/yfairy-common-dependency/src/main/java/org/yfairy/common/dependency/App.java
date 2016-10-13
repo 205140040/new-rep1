933d7372a1f1a69cae80619cedfd557f68f3c217
jiangzi <205140040@qq.com>
2016-10-13 22:19:18 +0800

    yfairy

---
+++ yfairy-all/yfairy-common-dependency/src/main/java/org/yfairy/common/dependency/App.java
@@ -0,0 +1,13 @@
+package org.yfairy.common.dependency;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
