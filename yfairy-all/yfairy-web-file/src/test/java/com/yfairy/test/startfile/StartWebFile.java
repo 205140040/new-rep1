2a74e5b278ef57cf992121a9265fae6dafce9a09
jiangzi <205140040@qq.com>
2016-11-05 18:09:36 +0800

    把dubbo-provider打成jarkeng

--- yfairy-all/yfairy-web-file/src/test/java/com/yfairy/test/startfile/StartWebFile.java
+++ yfairy-all/yfairy-web-file/src/test/java/com/yfairy/test/startfile/StartWebFile.java
@@ -1,11 +1,11 @@
-package com.yfairy.test.startfile;
-
-import com.yfairy.common.jetty.StartJetty;
-
-public class StartWebFile {
-
-	public static void main(String[] args) {
-		StartJetty.startJetty(8081, "/yfairy-web-file");
-	}
-
-}
+//package com.yfairy.test.startfile;
+//
+//import com.yfairy.common.jetty.StartJetty;
+//
+//public class StartWebFile {
+//
+//	public static void main(String[] args) {
+//		StartJetty.startJetty(8081, "/yfairy-web-file");
+//	}
+//
+//}
