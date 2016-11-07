a5b6384791bc06efc80e8896bcc3139d8522bc34
jiangzi <205140040@qq.com>
2016-11-07 22:54:40 +0800

    aaaaaaaaaaa

---
+++ yfairy-all/yfairy-demo/src/main/java/com/yfairy/demo/poi/ExcelDemo.java
@@ -0,0 +1,19 @@
+package com.yfairy.demo.poi;
+
+public class ExcelDemo {
+
+	public static void main(String[] args) {
+		/**
+		 * HSSF is the POI Project's pure Java implementation of the Excel
+		 * '97(-2007) file format. <br>
+		 * 1.HSSF普通excel.xls的实现 XSSF is the POI <br>
+		 * Project's pure Java implementation of the Excel 2007 OOXML (.xlsx)
+		 * file format. <br>
+		 * 2.XSSF是POI Project的纯Java实现的Excel 2007 OOXML（.xlsx）文件格式。
+		 * 3.忙碌的开发者指南HSSF和XSSF功能：
+		 * https://poi.apache.org/spreadsheet/quick-guide.html
+		 */
+
+	}
+
+}
--- yfairy-all/yfairy-demo/src/main/java/com/yfairy/demo/poi/PoiDemo.java
+++ yfairy-all/yfairy-demo/src/main/java/com/yfairy/demo/poi/PoiDemo.java
@@ -3,6 +3,11 @@ package com.yfairy.demo.poi;
 public class PoiDemo {
 
 	public static void main(String[] args) {
+		/**
+		 * Apache POI 是用Java编写的免费开源的跨平台的 Java API，Apache
+		 * POI提供API给Java程式对Microsoft Office格式档案读和写的功能。POI为“Poor Obfuscation
+		 * Implementation”的首字母缩写，意为“可怜的模糊实现”。
+		 */
 		System.out.println("hello");
 	}
 
