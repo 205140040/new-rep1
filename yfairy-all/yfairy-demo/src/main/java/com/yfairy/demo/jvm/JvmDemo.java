c3166c0bdd21a0c2b3475e4ff185c35d8b29e6b4
jiangzi <205140040@qq.com>
2016-12-05 00:14:50 +0800

    1111111111111

---
+++ yfairy-all/yfairy-demo/src/main/java/com/yfairy/demo/jvm/JvmDemo.java
@@ -0,0 +1,15 @@
+package com.yfairy.demo.jvm;
+
+public class JvmDemo {
+
+	public static void main(String[] args) {
+		/**
+		 * 深入理解java虚拟机第2版
+		 * 1.6实战自己编译jdk,22(43/460)
+		 * 2.4，实战outofmemory50(71/460)
+		 */
+		System.out.println("hello");
+
+	}
+
+}
