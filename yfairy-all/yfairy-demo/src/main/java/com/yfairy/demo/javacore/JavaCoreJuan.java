bc6d39d3331420a3dffad1a93866f8dfcd175a2f
jiangzi <205140040@qq.com>
2016-12-24 19:24:06 +0800

    66666666

---
+++ yfairy-all/yfairy-demo/src/main/java/com/yfairy/demo/javacore/JavaCoreJuan.java
@@ -0,0 +1,21 @@
+package com.yfairy.demo.javacore;
+
+public class JavaCoreJuan {
+
+	public static void main(String[] args) {
+		/**
+		 * java核心技术卷<br>
+		 * 第1章java程序设计概述17/720<br>
+		 * 第2章java程序设计环境29/720<br>
+		 * 安装java源文件和文档，即源码，<br>
+		 * cd jdk path<br>
+		 * mkdir src<br>
+		 * cd src<br>
+		 * jar xvf ../src.zip或windows中执行jar xvf ..\src.zip<br>
+		 * 2.2选择开发环境34/720<br>
+		 * 
+		 */
+		System.out.println("JavaCoreJuan");
+	}
+
+}
