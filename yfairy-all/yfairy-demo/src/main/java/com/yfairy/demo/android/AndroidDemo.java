889f6b76ae5e7fee09ac5624f08ef067fb0c654e
jiangzi <205140040@qq.com>
2017-03-11 23:37:56 +0800

    55555555555555

--- yfairy-all/yfairy-demo/src/main/java/com/yfairy/demo/android/AndroidDemo.java
+++ yfairy-all/yfairy-demo/src/main/java/com/yfairy/demo/android/AndroidDemo.java
@@ -5,6 +5,24 @@ public class AndroidDemo {
 	public static void main(String[] args) {
 		/**
 		 * 机器人<br>
+		 * 官网:https://developer.android.com<br>
+		 * 1.2.2搭建开发环境，Android sdk，ADT<br>
+		 * Eclipse+ADT+Android SDK 搭建安卓开发环境<br>
+		 * http://www.mamicode.com/info-detail-516839.html<br>
+		 * sdk下载网址:https://android-sdk.cn.uptodown.com/windows/download<br>
+		 * <br>
+		 * launch 英[lɔ:ntʃ]<br>
+		 * 
+		 * vt. 发射; 发动; [计算机]开始（应用程序）; 开展（活动、计划等）;<b<br>
+		 * r> vi. 投入; 着手进行; 热衷于…;<br>
+		 * n. 投掷; 大船上的小艇; 大型敞篷摩托艇;<br>
+		 * 
+		 * <br>
+		 * 给Android Studio设置代理<br>
+		 * http://blog.csdn.net/lchad/article/details/43567675<br>
+		 * 
+		 * 快速Android开发系列网络篇之Android-Async-Http<br>
+		 * http://www.cnblogs.com/angeldevil/p/3729808.html<br>
 		 */
 
 	}
