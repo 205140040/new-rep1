39da5a7ff9c4597a4b9006b10f688042b1543c0f
jiangzi <205140040@qq.com>
2017-04-24 23:57:49 +0800

    wuyuele

--- yfairy-all/yfairy-mybatis-generator/src/main/java/com/yfairy/mybatis/generator/App.java
+++ yfairy-all/yfairy-mybatis-generator/src/main/java/com/yfairy/mybatis/generator/App.java
@@ -7,6 +7,7 @@ package com.yfairy.mybatis.generator;
 public class App {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+		System.err.println("githubba kengkengkeng");
 		/**
 		 * http://mbg.cndocs.tk/running/runningWithMaven.html
 		 * 使用命令:mybatis-generator:generate
